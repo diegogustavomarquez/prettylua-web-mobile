eb35940050796503952c5bf28e5dac540a5e0591
diegogustavomarquez <getcoffee.ar@gmail.com>
2022-06-04 13:48:34 -0300

    se agrega la pantalla y el ruteo a la pantalla de mascota

---
+++ src/app/pages/main/pet/pet-view/pet-view.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-view',
+  templateUrl: './pet-view.page.html',
+  styleUrls: ['./pet-view.page.scss'],
+})
+export class PetViewPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
