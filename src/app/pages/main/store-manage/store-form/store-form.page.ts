f2e23bd87e67402ed61722f2c30cfb0a0a20b2de
diegogustavomarquez <getcoffee.ar@gmail.com>
2022-06-15 14:28:44 -0300

    se agrega desarollo

--- src/app/pages/main/store-manage/store-form/store-form.page.ts
+++ src/app/pages/main/store-manage/store-form/store-form.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Store, Usuario } from 'src/app/interfaces/interfaces';
 import { CommonsService } from 'src/app/services/commons.service';
+import { UiServiceService } from 'src/app/services/ui-service.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 import { StoreService } from '../../../../services/store.service';
 
@@ -14,14 +15,17 @@ export class StoreFormPage implements OnInit {
 
   store: Store = {};
   usuario: Usuario = {};
-  provincias : string[] = [];
-  servicios :string[]=[];
-  dias :string[]=[];
+  provincias: string[] = [];
+  servicios: string[] = [];
+  dias: string[] = [];
 
+  imageTemporal: any;
+  isPhotoPresent: boolean = false;
 
-  constructor(private commonsService : CommonsService,
-              private usuarioService: UsuarioService,
-              private storeService: StoreService) { }
+  constructor(private commonsService: CommonsService,
+    private usuarioService: UsuarioService,
+    private storeService: StoreService,
+    private uiService: UiServiceService) { }
 
   ngOnInit() {
     this.provincias = this.commonsService.getProvincias();
@@ -29,9 +33,47 @@ export class StoreFormPage implements OnInit {
     this.dias = this.commonsService.getDias();
     this.usuario = this.usuarioService.usuario;
     this.storeService.findByUserId(this.usuario._id).then(p => p ? this.store = p : p = {});
+    if (this.store && this.store.promocionFoto) {
+      this.isPhotoPresent = true;
+      this.imageTemporal = this.store.promocionFoto;
+    }
   }
 
   async crearStore(fStore: NgForm) {
   }
+
+
+  /**
+ * carga la imagen elegida por el usuario
+ * 
+ * @param event 
+ */
+  async loadImagen(event: any) {
+    let archivos = event.target.files[0];
+    let sizeFile: number = archivos.size;
+    console.log(sizeFile);
+    /**
+    if (sizeFile > 100000) {
+      this.uiService.alertaInformativa('La imagen que intenta guardar tiene mucha resolución.');
+      return;
+    } */
+    let reader = new FileReader();
+    reader.readAsDataURL(archivos);
+    reader.onloadend = () => {
+      this.imageTemporal = reader.result as string;
+      this.isPhotoPresent = true;
+    }
+  }
+
+  /**
+ * Saca la foto seleccionada
+ * 
+ * @param event 
+ */
+  async kickImagen() {
+    this.imageTemporal = null;
+    this.isPhotoPresent = false;
+  }
+
 
 }
